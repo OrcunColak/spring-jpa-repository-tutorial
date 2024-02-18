6d44c36412a8b6c55090f6e41a7ab2ac601dcd52
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-18 09:40:09 +0300

    examples for custom method

--- src/main/java/com/colak/springjparepositorytutorial/jpa/Product.java
+++ src/main/java/com/colak/springjparepositorytutorial/jpa/Product.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "products")
 
+@Getter
+@Setter
 @ToString
 public class Product {
 
